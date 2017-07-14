129c9d1febdc61dfd3ee52c479208a312315c8f5
DongHwa Shin <dhshin@hcil.snu.ac.kr>
2017-07-14 09:21:47 +0900

    2293 complete

--- 2293/main.cpp
+++ 2293/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
+#define MAXK 10001
+int comb[MAXK];
+
 int main()
 {
+	int n, k, coin;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> coin;
+		for (int j = 0; j <= k; j++)
+		{
+			if (i == 0)
+			{
+				if (j % coin == 0)
+				{
+					comb[j] = 1;
+				}
+			}
+			else
+			{
+				int prev = j - coin;
+				if (prev >= 0)
+					comb[j] += comb[prev];
+			}
+		}
+	}
+	cout << comb[k];
 	return 0;
 }
