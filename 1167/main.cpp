837b75ff153dca515ab03ec62b044990b58f9e12
DongHwa Shin <dhshin@hcil.snu.ac.kr>
2017-08-10 16:45:30 +0900

    효율적인 해킹

---
+++ 1167/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int V;
+
+int main()
+{
+	scanf("%d", &V);
+
+	return 0;
+}
--- 1325/main.cpp
+++ 1325/main.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 #define MAXN 10001
 
@@ -10,63 +11,67 @@ int MaxNum;
 int front, rear;
 int Queue[MAXN];
 
-class Node{
+class DArray{
 public:
-	int value;
-	Node* next;
-	Node(int value)
+	int used;
+	int size;
+	int* values;
+	DArray(){
+		used = 0;
+		size = 1;
+		values = new int[size];
+	}
+	~DArray(){
+		delete[] values;
+	}
+
+	void push_back(int data){
+		if (this->used == this->size) {
+			int* temp = new int[this->size];
+			for (int i = 0; i < this->used; i++)
+				temp[i] = this->values[i];
+
+			delete[] this->values;
+			this->size *= 2;
+			this->values = new int[this->size];
+
+			for (int i = 0; i < this->used; i++)
+				this->values[i] = temp[i];
+			delete[] temp;	
+		}
+		this->values[used] = data;
+		this->used++;
+	}
+
+	int get(int index)
 	{
-		this->value = value;
-		next = NULL;
+		return this->values[index];
+	}
+
+	void print_all()
+	{
+		for (int i = 0; i < this->used; i++)
+			cout << this->values[i] << " ";
+		cout << endl;
 	}
 };
 
-Node* AdjList[MAXN];
-Node* LastOne[MAXN];
+DArray* AdjList;
 
 void Input()
 {
-	scanf("%d %d", &N, &M);
-	for (int i = 1; i <= N; i++)
-		AdjList[i] = NULL;
-
+	cin >> N >> M;
+	AdjList = new DArray[N + 1];
 	for (int i = 1; i <= M; i++)
 	{
 		int a, b;
-		scanf("%d %d", &a, &b);
+		cin >> a >> b;
 		// a가 b를 신뢰한다
 		// == b를 감염시키면 a도 감염된다
 		// == b -> a
-
-		if (AdjList[b] == NULL)
-		{
-			AdjList[b] = new Node(a);
-			LastOne[b] = AdjList[b];
-		}
-		else{
-			Node* ptr = LastOne[b];
-			ptr->next = new Node(a);
-			LastOne[b] = ptr->next;
-		}
+		AdjList[b].push_back(a);
 	}
 }
-
-/*
-int DFS(int a)
-{
-	int count = 0;
-
-	Visit[a] = 1;
-	count++;
-
-	for (int i = 1; i <= N; i++)
-	{
-		if (i != a && Mat[a][i] == 1 && Visit[i] == 0)
-			count += DFS(i);
-	}
-	return count;
-}
-*/
 
 void Enqueue(int a)
 {
@@ -87,19 +92,14 @@ void BFS(int a)
 		int start = Queue[front];
 		front++;
 
-		Node* ptr = AdjList[start];
-		while (1)
+		for (int i = 0; i < AdjList[start].used; i++)
 		{
-			if (ptr == NULL){
-				break;
-			}
-			int adj = ptr->value;
+			int adj = AdjList[start].get(i);
 			if (Visit[adj] == 0)
 			{
 				Enqueue(adj);
 				Visit[adj] = 1;
 			}
-			ptr = ptr->next;
 		}
 	}
 }
@@ -108,21 +108,6 @@ void ClearVisit()
 {
 	for (int i = 1; i <= N; i++)
 		Visit[i] = 0;
-}
-
-void PrintAdjList(){
-	for (int i = 1; i <= N; i++){
-		Node* ptr = AdjList[i];
-		if (ptr == NULL)
-			continue;
-		printf("%d : ", i);
-		while (ptr != NULL)
-		{
-			printf("%d ", ptr->value);
-			ptr = ptr->next;
-		}
-		printf("\n");
-	}
 }
 
 void Process()
@@ -143,16 +128,14 @@ void Output()
 	for (int i = 1; i <= N; i++)
 	{
 		if (Num[i] == MaxNum)
-			printf("%d ", i);
+			cout << i << " ";
 	}
 }
 
 int main()
 {
 	Input();
-	//PrintAdjList();
 	Process();
 	Output();
-	//scanf("%d", &N);
 	return 0;
 }
