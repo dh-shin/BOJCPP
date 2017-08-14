e7b46baf197c61c8079a3758998aa6b8219526db
DongHwa Shin <dhshin@hcil.snu.ac.kr>
2017-08-14 16:28:27 +0900

    이중 우선순위큐 GG

--- 7662/main.cpp
+++ 7662/main.cpp
@@ -1,43 +1,50 @@
 #include <iostream>
 using namespace std;
 
+#define MAXN 1000001
 
-class MinQ{
-
+class QItem{
+public:
+	long long int num;
+	int idx;
+	QItem(){
+	}
 };
 
-class MaxQ{
+class MinQ{
 public:
 	int size;
-	int* data;
 	int used;
-	int pop;
-	MaxQ(){
+	QItem** data;
+
+	MinQ(){
 		size = 128;
 		used = 0;
-		data = new int[size];
+		data = new QItem*[size];
 	}
-	~MaxQ(){
+
+	~MinQ(){
 		delete[] data;
 	}
 
-	void enqueue(int item){
+	void enqueue(QItem* item){
 		used++;
 		if (used == size)
 		{
-			int* temp = new int[size];
+			QItem** temp = new QItem*[size];
 			for (int i = 1; i < size; i++)
 				temp[i] = data[i];
 			delete[] data;
-			data = new int[size * 2];
+			data = new QItem*[size * 2];
 			for (int i = 1; i < size; i++)
 				data[i] = temp[i];
 			delete[] temp;
 			size *= 2;
 		}
+
 		int curr = used;
 		int par = curr / 2;
-		while (curr != 1 && data[par] < item)
+		while (curr != 1 && (data[par]->num > item->num))
 		{
 			data[curr] = data[par];
 			curr = par;
@@ -46,29 +53,29 @@ public:
 		data[curr] = item;
 	}
 
-	bool dequeue()
+	QItem* dequeue()
 	{
 		if (used == 0)
-			return false;
-		
-		pop = data[1]; // 정해진 반환값 지정
-		used--;
+			return NULL;
 
+		QItem* pop = data[1]; // 정해진 반환값 지정
+		data[1] = data[used]; // 말단을 루트로 옮기기
+		used--; // 아이템 수 하나 줄이기
+
+		QItem* move_item = data[1];
 		int curr = 1;
-		data[curr] = data[used]; // 말단을 루트로 옮기기
-		int curr_item = data[curr];
 		while (1)
 		{
 			int child = curr * 2;
 			if (child > used) // 자식이 없는 경우 (끝)
-				break; 
+				break;
 
-			// 자식이 둘다 있고, 둘 중 child + 1 이 더 큰 놈일 경우
-			if (child + 1 <= used && data[child] < data[child + 1])
+			// 자식이 둘다 있고, 둘 중 child + 1 이 더 작은 놈일 경우
+			if (child + 1 <= used && (data[child]->num > data[child + 1]->num))
 				child++;
 
-			// 자식이 더 클 경우
-			if (data[child] > curr_item)
+			// 자식이 더 작을 경우
+			if (data[child]->num < move_item->num)
 			{
 				data[curr] = data[child];
 				curr = child;
@@ -77,43 +84,180 @@ public:
 				break;
 			}
 		}
-		data[curr] = curr_item;
-		return true;
+		data[curr] = move_item;
+		return pop;
+	}
+
+	void print_all()
+	{
+		cout << "MINQ : ";
+		for (int i = 1; i <= used; i++)
+			cout << data[i]->num << " ";
+		cout << endl;
+	}
+};
+
+class MaxQ{
+public:
+	int size;
+	int used;
+	QItem** data;
+
+	MaxQ(){
+		size = 128;
+		used = 0;
+		data = new QItem*[size];
+	}
+
+	~MaxQ(){
+		delete[] data;
+	}
+
+	void enqueue(QItem* item){
+		used++;
+		if (used == size)
+		{
+			QItem** temp = new QItem*[size];
+			for (int i = 1; i < size; i++)
+				temp[i] = data[i];
+			delete[] data;
+			data = new QItem*[size * 2];
+			for (int i = 1; i < size; i++)
+				data[i] = temp[i];
+			delete[] temp;
+			size *= 2;
+		}
+
+		int curr = used;
+		int par = curr / 2;
+		while (curr != 1 && (data[par]->num < item->num))
+		{
+			data[curr] = data[par];
+			curr = par;
+			par = curr / 2;
+		}
+		data[curr] = item;
+	}
+
+	QItem* dequeue()
+	{
+		if (used == 0)
+			return NULL;
+
+		QItem* pop = data[1]; // 정해진 반환값 지정
+		data[1] = data[used]; // 말단을 루트로 옮기기
+		used--; // 아이템 수 하나 줄이기
+
+		QItem* move_item = data[1];
+		int curr = 1;
+		while (1)
+		{
+			int child = curr * 2;
+			if (child > used) // 자식이 없는 경우 (끝)
+				break;
+
+			// 자식이 둘다 있고, 둘 중 child + 1 이 더 큰 놈일 경우
+			if (child + 1 <= used && (data[child]->num < data[child + 1]->num))
+				child++;
+
+			// 자식이 더 클 경우
+			if (data[child]->num > move_item->num)
+			{
+				data[curr] = data[child];
+				curr = child;
+			}
+			else{ // 자신이 자식보다 더 크거나 같을 경우 (끝)
+				break;
+			}
+		}
+		data[curr] = move_item;
+		return pop;
+	}
+
+	void print_all()
+	{
+		cout << "MAXQ : ";
+		for (int i = 1; i <= used; i++)
+			cout << data[i]->num << " ";
+		cout << endl;
 	}
 };
 
 int k;
 MaxQ* maxQ;
+MinQ* minQ;
+int visit[MAXN];
 
 void Input()
 {
 	maxQ = new MaxQ();
+	minQ = new MinQ();
 
 	cin >> k;
+	int idx = 0;
 	while (k--)
 	{
-		int curr;
-		cin >> curr;
-		maxQ->enqueue(curr);
+		char op[3];
+		int num;
+		cin >> op >> num;
+		if (op[0] == 'I')
+		{
+			QItem* curr = new QItem();
+			curr->num = num;
+			curr->idx = ++idx;
+			visit[idx] = 0;
+			maxQ->enqueue(curr);
+			minQ->enqueue(curr);
+			//maxQ->print_all();
+		    //minQ->print_all();
+		}
+		else // D
+		{
+			if (num == 1)
+			{
+				QItem* item = maxQ->dequeue();
+				while (item != NULL && visit[item->idx] == 1)
+					item = maxQ->dequeue();
+
+				if (item == NULL)
+					continue;
+
+				visit[item->idx] = 1;
+				//cout << "POP : " << item->num << endl;
+				//maxQ->print_all();
+				//minQ->print_all();
+			}
+			else
+			{
+				QItem* item = minQ->dequeue();
+				while (item != NULL && visit[item->idx] == 1)
+					item = maxQ->dequeue();
+				
+				if (item == NULL)
+					continue;
+
+				visit[item->idx] = 1;
+				//cout << "POP : " << item->num << endl;
+				//maxQ->print_all();
+				//minQ->print_all();
+			}
+		}
 	}
 
-	while (maxQ->dequeue() == true)
-	{
-		int output = maxQ->pop;
-		cout << output << endl;
-	}
-	
+	QItem* max_item = maxQ->dequeue();
+	while (max_item != NULL && visit[max_item->idx] == 1)
+		max_item = maxQ->dequeue();
+	QItem* min_item = minQ->dequeue();
+	while (min_item != NULL && visit[min_item->idx] == 1)
+		min_item = maxQ->dequeue();
+
+	if (max_item == NULL && min_item == NULL)
+		cout << "EMPTY" << endl;
+	else
+		cout << max_item->num << " " << min_item->num << endl;
+
 	delete maxQ;
-}
-
-void Process()
-{
-
-}
-
-void Output()
-{
-
+	delete minQ;
 }
 
 int main()
