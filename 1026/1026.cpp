8cf083b55ff6eb9fe540ae13a0508b5c88385e52
DongHwa Shin <dhshin@hcil.snu.ac.kr>
2017-08-25 03:00:16 +0900

    1026

---
+++ 1026/1026.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+#define MAXN 50
+#define LIMIT 100
+
+int N;
+int A[MAXN + 1];
+int B[MAXN + 1];
+int Count[LIMIT + 1];
+
+int main()
+{
+	cin >> N;
+	int num;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> num;
+		A[num]++;
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> num;
+		B[num]++;
+	}
+
+	int sum = 0;
+	int idxa = 0, idxb = 100;
+	for (int i = 1; i <= N; i++)
+	{
+		while (1)
+		{
+			if (A[idxa] != 0)
+			{
+				A[idxa]--;
+				break;
+			}
+			idxa++;
+		}
+		while (1)
+		{
+			if (B[idxb] != 0)
+			{
+				B[idxb]--;
+				break;
+			}
+			idxb--;
+		}
+		sum += idxa * idxb;
+	}
+
+	cout << sum << endl;
+	return 0;
+}
